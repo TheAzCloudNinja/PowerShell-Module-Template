1bb8530bba27bb91f317ec67027dfd852af6f250
The Azure Cloud Ninja <TheAzCloudNinja@hotmail.com>
2020-10-14 10:54:02 +0200

    Update PsScript ignore list

--- Plaster_template/module/module.PSSATests.ps1
+++ Plaster_template/module/module.PSSATests.ps1
@@ -26,6 +26,16 @@ if (Test-Path -Path "$modulePath\Public\*.ps1") {
     $functionPaths += Get-ChildItem -Path "$modulePath\Public\*.ps1" -Exclude "*.Tests.*"
 }
 
+
+#setup analyzer ignore rules
+$scriptAnalyzerRulesIgnore = $null
+$AnalyzerFile = Get-ChildItem -Path $modulePath -Include AnalyzerRulesIgnore.csv -File -Recurse -ErrorAction SilentlyContinue
+If ($AnalyzerFile) {
+
+    #Setting exclusion rules for PS Script analyzer
+    $scriptAnalyzerRulesIgnore = Get-Content -Path $AnalyzerFile[0].FullName
+}
+
 # Running the analysis for each function
 foreach ($functionPath in $functionPaths) {
     $functionName = $functionPath.BaseName
@@ -33,14 +43,20 @@ foreach ($functionPath in $functionPaths) {
     Describe "'$functionName' Function Analysis with PSScriptAnalyzer" {
         Context 'Standard Rules' {
             # Define PSScriptAnalyzer rules
+
             $scriptAnalyzerRules = Get-ScriptAnalyzerRule # Just getting all default rules
 
             # Perform analysis against each rule
             forEach ($rule in $scriptAnalyzerRules) {
-                It "should pass '$rule' rule" {
-                    $results = Invoke-ScriptAnalyzer -Path $functionPath -IncludeRule $rule 
-                    $results = $results | Out-String                    
-                    $results | should BeNullOrEmpty
+                if (!($scriptAnalyzerRulesIgnore -contains $($rule.RuleName))) {
+                    It "should pass '$rule' rule" {
+                        $results = Invoke-ScriptAnalyzer -Path $functionPath -IncludeRule $rule 
+                        $results = $results | Out-String                    
+                        $results | should BeNullOrEmpty
+                    }
+                }
+                else {
+                    Write-Warning -Message "The $rule is ignored for file $functionPath"
                 }
             }
         }
